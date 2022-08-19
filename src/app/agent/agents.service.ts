74f2f90d98fabd721d86ea9fb6c48162444f4c65
betulbas <82716583+betulbas@users.noreply.github.com>
2022-08-19 19:32:42 +0300

    Update agents.service.ts

--- src/app/agent/agents.service.ts
+++ src/app/agent/agents.service.ts
@@ -8,9 +8,6 @@ import { AddAgentRequest } from '../models/api-models/addAgentRequest.model';
 import { Agents } from '../models/api-models/agents.model';
 import { updateAgentRequest } from '../models/api-models/updateAgentRequest.model';
 
-import {map} from 'rxjs/operators';
-//import localStorage from 'local-storage';
-import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
@@ -20,8 +17,7 @@ export class AgentsService {
   loginUrl : string = '';
   signUpUrl : string = '';
 
- // jwtHelper=new JwtHelperService();
-//  decodedToken:any;
+
 
 
   constructor(private httpClient:HttpClient) { 
@@ -40,14 +36,7 @@ export class AgentsService {
      const headers = {'content-type': 'application/json'};
    return this.httpClient.get(this.baseApiUrl+"/api/mustem/agentid/"+t); }
  
-/* getSicil(agentidu:string |null): Observable<Agents[]>{
-    return this.httpClient.get<Agents[]>("http://localhost:8080/api/mustem/agentid/"+agentidu);
-  }
 
-getPerformancesByAgentId(t:any): Observable<any> { 
-  return this.httpClient.get(this.baseApiUrl+"/agentid/"+t)
-  .pipe(map( res => { if (res) { return res; } else { return {}; } } )); }
-*/
   updateAgent(mustemId:string,agentRequest:Agents): Observable<Agents>{
     const updateAgentRequest : updateAgentRequest={
       id:agentRequest.id,
@@ -92,42 +81,7 @@ getPerformancesByAgentId(t:any): Observable<any> {
 
 
     }
-  //  const SicilNO = user; 
-  /*
-   login(userRequest : User) : Observable<User> {
-   const addUserRequest:AddUserRequest ={
-      agentid:userRequest.agentid,
-      firstname:userRequest.firstname,
-      surname:userRequest.surname,
-      password:userRequest.password,
-      role:userRequest.role
-      
-return this.httpClient.post<User>(this.loginUrl,addUserRequest);
-  */
-    
-   
-    //console.log(this.user)
-  
-/*
-login(user : User) : Observable<any> {
-    return this.httpClient.post<any>(this.loginUrl,user);
 
-
-
-
-login(user:User){
-  return this.httpClient.post(this.loginUrl,user).pipe(
-    map((response:any)=>{
-      const result = response;
-      if(result){
-        localStorage.setItem("token",result.token);
-        this.decodedToken=this.jwtHelper.decodeToken(result.token);
-        console.log(this.decodedToken);
-      }
-    })
-  )
-}
-*/
 
   signUp(user : User) : Observable<any> {
     return this.httpClient.post<any>(this.signUpUrl,user);
